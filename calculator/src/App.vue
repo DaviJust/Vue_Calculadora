ba92a783b447e563a50e91fc9e55f299fa948c77
Davi <davijustinocouto@gmail.com>
2023-01-23 11:49:35 -0300

    components

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Calculadora</h1><CalculatorDisplay/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorDisplay from "./main/CalculatorDisplay.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{CalculatorDisplay}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@font-face{
+    font-family:"RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf")
 }
+
+*{
+    font-family: "RobotoMono", monospace;
+    background-color:linear-gradient(#e66465, #9198e5);
+}
+body{
+    margin:0;
+
+}
+#app{
+    display:flex;
+    flex-direction:column;
+    height:100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(#e66465, #9198e5);;
+}
+
 </style>
---
+++ calculator/src/main/CalculatorDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="calculator">
+    <CalculatorDisplayBg></CalculatorDisplayBg>
+    <CalculatorButton></CalculatorButton>
+    <CalculatorButton></CalculatorButton>
+    <CalculatorButton></CalculatorButton>
+    <CalculatorButton></CalculatorButton>
+    <CalculatorButton></CalculatorButton>
+    <CalculatorButton></CalculatorButton>
+  </div>
+  
+</template>
+
+<script>
+ import CalculatorButton from "../components/CalculatorButton.vue"
+ import CalculatorDisplayBg from "../components/CalculatorDisplayBg.vue"
+ export default {
+    components: {CalculatorButton, CalculatorDisplayBg},
+
+}
+
+</script>
+
+<style>
+
+.calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
